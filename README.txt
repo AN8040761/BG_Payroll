ff35e6991396e6d15c95f9478a5ec7d69d479d28
AN8040761 <nastev.alexander@gmail.com>
2017-11-01 07:36:42 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,16 @@
+This is an payroll accounting program build in accordance with Bulgarian social security laws.
+If you want to add new data use the forms danni_forma_76.html, danni_osiguren.html or danni_osiguritel.html.
+After each new entry click "Save", when ready - click download.
+If you want to start over click "Clear".
+
+If you execute the file read_arr.py this will generate a file EMPL2000.txt, based on the
+data in danni_forma_76.csv, danni_osiguren.csv and danni_osiguritel.csv.
+
+Currently only 17 of the total of 50 fields in EMPL2000.txt are filled with the required
+information. 
+
+If you have any questions related to the forms please visit:
+http://deklar1.getweb.cf/
+
+The python script has appropriate comments, while for now the HTML script is uncommented.
+Please share your thoughts if you have ideas for improvisation and clarification of the program.
---
+++ read_arr.py
@@ -0,0 +1,131 @@
+import csv
+
+'''The function read_arr takes as argument the name of a csv file without the extension 
+and returns a list of lists created by each row in the csv file'''
+def read_arr( arr_name ):
+  datafile = open(arr_name + '.csv', 'r')
+  datareader = csv.reader(datafile,delimiter=',')
+  arr_name = []
+  for row in datareader:
+    arr_name.append(row)
+  '''del arr_name [0]'''
+  print(arr_name)
+  return arr_name
+
+danni_osiguritel = read_arr("danni_osiguritel")
+danni_osiguren   = read_arr("danni_osiguren")
+danni_forma_76   = read_arr("danni_forma_76")
+col1 = [i[0] for i in danni_osiguren]
+print (col1)
+'''The folowing code performs an inner join of the two lists of lists danni_osiguren and danni_forma_76.
+The id_n column from danni_forma_76 is excluded from the join because it is available in danni_osiguren.
+Everithing is saved in the list of lists onsoven_masiv
+'''
+osnoven_masiv = []
+
+osnoven_masiv = [(i[:] + j[:2] + j[3:]) for i in danni_osiguren for j in danni_forma_76 if i[0] == j[2]]
+print(osnoven_masiv)
+'''The following code converts osnoven_masiv into the dictionary omd'''
+keys = osnoven_masiv[0]
+omd = [dict(zip(keys, values)) for values in osnoven_masiv[1:]]
+
+
+print(omd)
+
+
+'''The codes f1, f2 etc. represent the fields in the file EMPL2000.txt - a csv with structure and contents
+  predefined by the National Revenue Agency of Bulgaria'''
+for row in omd:
+
+    result = int(row['month'])
+    row.update( {'f1': result})
+
+    result = int(row['year'])
+    row.update( {'f2': result})
+    result = int(danni_osiguritel[1][1])
+    row.update( {'f3': result})
+    result = int(row['egn'])
+    row.update( {'f4': result})
+    result = int(row['flag'])
+    row.update( {'f5': result})
+    result = (row['last_name'])
+    row.update( {'f6': result})
+    result = (row['initials'])
+    row.update( {'f7': result})
+    result = int(row['type_ens'])
+    row.update( {'f12': result})
+    result = int(row['r']) + int(row['o']) + int(row['m']) + int(row['b']) + int(row['a'])
+    row.update( {'f24': result})
+    result = int(row['r'])
+    row.update( {'f25': result})
+
+    if (int(row['b']) <= 3):
+      result = 0
+      result1 = int(row['b'])
+    else:
+        result = int(row['b']) - 3
+        result1 = 3
+    row.update( {'f26': result})
+    result = int(row['m'])
+    row.update( {'f27': result})
+    result = int(row['a'])
+    row.update( {'f28': result})
+    result = danni_osiguritel [1][2]
+    row.update( {'f71': result})
+    result = (row['activity_code'])
+    row.update( {'f72': result})
+    result = int(row['proff_code'][0])
+    row.update( {'f73': result})
+    row.update( {'f94': result1})
+    '''etc.'''
+
+    '''This code converts the list of dictinaries omd into the list of lists osnoven_masiv, removes the first 22 columns
+    and then inserts 4 empty strings in the slice between elements 6 and 7 (starting from 0) i.e. f7 and f12.
+    Then writes the list of lists into a csv file called EMPL2000.txt'''
+    osnoven_masiv = [list(d.values()) for d in omd]
+
+    for row in osnoven_masiv:
+        del row [:22]
+        row[7:7] = ["","","",""]
+
+    with open("EMPL2000.txt", "w") as f:
+      writer = csv.writer(f)
+      writer.writerows(osnoven_masiv)
+
+
+
+
+print(omd)
+print(osnoven_masiv)
+
+'''
+ result = float(row['id_n']) + float(row['id_n']) * float(row['id_n'])
+    row.update( {'f233': '%.2f' % result})
+    row.update({'item4': 4, 'item5': 5})
+
+ keys = omd[0].keys()
+ with open('omd.csv', 'w') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(omd)
+datafile = open('danni_osiguren.csv', 'r')
+
+datareader = csv.reader(datafile,delimiter=',')
+arr_name = []
+
+for row in datareader:
+    arr_name.append(row)
+del arr_name [0]
+for value in arr_name:
+    solution = float(value[0]) + float(value[0]) * float(value[0])
+    value.insert(7, '%.2f' % solution)
+
+print(arr_name)'''
+
+'''https://stackoverflow.com/questions/25417184/multiply-2-elements-in-each-row-create-new-column-list-of-lists-python
+https://stackoverflow.com/questions/1859864/how-to-create-an-integer-array-in-python
+https://stackoverflow.com/questions/4575973/python-conditional-list-creation-from-2d-lists
+https://stackoverflow.com/questions/6740918/creating-a-dictionary-from-a-csv-file
+https://stackoverflow.com/questions/2646157/what-is-the-fastest-to-access-struct-like-object-in-python
+https://stackoverflow.com/questions/3086973/how-do-i-convert-this-list-of-dictionaries-to-a-csv-file-python
+'''
